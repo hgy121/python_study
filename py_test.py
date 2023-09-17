c4ba6ee968592f422905ea3eccfaa5e6fb2ce6cd
hgy121 <geunyoung8121@naver.com>
2023-09-17 21:10:10 +0900

    init

--- py_test.py
+++ py_test.py
@@ -1,9 +1,11 @@
 
-print('gugudan')
+print('gugudan1=================')
 
 for i in range(2,10):
     for j in range(1, 10):
         print(f'{i}*{j}={i*j}')
 
+print('gugudan2================')
 
-print('end')
+l = [ f'{i}*{j}={i*j}' for i in range(2, 10) for j in range(1, 10) ]
+print(*l, sep='\n')
